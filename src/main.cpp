d54633ec4f68571a2dad4e94bc60c3862bca9e41
zivtzur <ziv.tzur@live.biu.ac.il>
2018-12-12 22:40:55 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 //load script file using lexer function and then parse using parser function.
 int main() {
     std::string line;
+    std::cout << "pull";
     Interpreter i;
     do {
         std::getline(std::cin, line);
