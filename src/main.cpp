10390a1b4f6ca8e25aaf6a91bf1b7cf06c9ddb18
elyawy <elya.wygoda@gmail.com>
2018-12-12 23:02:55 +0200

    added jello

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,11 @@
 //load script file using lexer function and then parse using parser function.
 int main() {
     std::string line;
-    std::cout << "pull";
     Interpreter i;
     do {
         std::getline(std::cin, line);
         i.lexer(line);
     } while (line != "`");
-    std::cout << "jello";
     return 0;
 }
 
