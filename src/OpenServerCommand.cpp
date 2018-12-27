138a94f570e79a8937f387342838cc0ba378975b
zivtzur <ziv.tzur@live.biu.ac.il>
2018-12-27 18:07:32 +0200

    - add Thread to OpenDataServer

--- src/OpenServerCommand.cpp
+++ src/OpenServerCommand.cpp
@@ -15,40 +15,19 @@
 #include <sys/socket.h>
 #include <sstream>
 #include <iostream>
+#include <thread>
+#include <algorithm>
 
 
 void OpenServerCommand::execute() {
     double portno = this->mapH.getExpressions()->at(this->mapH.getparseQueue()->front())->calculate(mapH);
+    jump();
+    double sleepTime = 1000 / this->mapH.getExpressions()->at(this->mapH.getparseQueue()->front())->calculate(mapH);
     if (portno == 5400) {
         std::cout << "the server is open!\n";
     }
     int sockfd, newsockfd, clilen;
-    char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
-    double  n;
-    std::vector<std::string> var = {"/instrumentation/airspeed-indicator/indicated-speed-kt",
-                                    "/instrumentation/altimeter/indicated-altitude-ft",
-                                    "/instrumentation/altimeter/pressure-alt-ft",
-                                    "/instrumentation/attitude-indicator/indicated-pitch-deg",
-                                    "/instrumentation/attitude-indicator/indicated-roll-deg",
-                                    "/instrumentation/attitude-indicator/internal-pitch-deg",
-                                    "/instrumentation/attitude-indicator/internal-roll-deg",
-                                    "/instrumentation/encoder/indicated-altitude-ft",
-                                    "/instrumentation/encoder/pressure-alt-ft",
-                                    "/instrumentation/gps/indicated-altitude-ft",
-                                    "/instrumentation/gps/indicated-ground-speed-kt",
-                                    "/instrumentation/gps/indicated-vertical-speed",
-                                    "/instrumentation/heading-indicator/indicated-heading-deg",
-                                    "/instrumentation/magnetic-compass/indicated-heading-deg",
-                                    "/instrumentation/slip-skid-ball/indicated-slip-skid",
-                                    "/instrumentation/turn-indicator/indicated-turn-rate",
-                                    "/instrumentation/vertical-speed-indicator/indicated-speed-fpm",
-                                    "/controls/flight/aileron",
-                                    "/controls/flight/elevator",
-                                    "/controls/flight/rudder",
-                                    "/controls/flight/flaps",
-                                    "/controls/engines/engine/throttle",
-                                    "/engines/engine/rpm"};
 
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -62,7 +41,7 @@ void OpenServerCommand::execute() {
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(static_cast<uint16_t>(portno));
 
     /* Now bind the host address using bind() call.*/
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
@@ -84,50 +63,9 @@ void OpenServerCommand::execute() {
         perror("ERROR on accept");
         exit(1);
     }
+    std::thread fromServer(&OpenServerCommand::connectAndUpdate, this,  sleepTime,  sockfd);
+    fromServer.detach();
 
-    /* If connection is established then start communicating */
-    while (true) {
-        bzero(buffer, 256);
-        n = read(newsockfd, buffer, 255);
-
-        if (n < 0) {
-            perror("ERROR reading from socket");
-            exit(1);
-        }
-
-        /* Push all argument from server to a vector */
-        std::istringstream iss(buffer);
-        std::queue<std::string> fromSer;
-        while(!iss.eof()) {
-            int i = 1;
-            std::string temp = "";
-            iss >> temp;
-            std::cout << temp << "\n";
-            std::string delim = ",";
-            auto start = 0U;
-            auto end = temp.find(delim);
-            while (end != std::string::npos)
-            {
-                fromSer.push(temp.substr(start, end - start));
-                start = end + delim.length();
-                end = temp.find(delim, start);
-
-            }
-            while (!fromSer.empty()) {
-                std::cout << var.at(i) << " -> " << fromSer.front() << "\n";
-                fromSer.pop();
-                i++;
-            }
-        }
-
-        /* Write a response to the client */
-        n = write(newsockfd, "I got your message", 18);
-
-        if (n < 0) {
-            perror("ERROR writing to socket");
-            exit(1);
-        }
-    }
 }
 
 void OpenServerCommand::addMaps(mapHandler &mapHandler1) {
@@ -136,6 +74,68 @@ void OpenServerCommand::addMaps(mapHandler &mapHandler1) {
 
 std::string OpenServerCommand::stringify() {
     return std::__cxx11::string();
+}
+
+void OpenServerCommand::connectAndUpdate(int sleepTime, int sockeNum) {
+    char buffer[256];
+    double  n;
+    /* If connection is established then start communicating */
+    while (true) {
+        bzero(buffer, 256);
+        n = read(sockeNum, buffer, 255);
+
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+
+        /* Push all argument from server to a vector */
+        std::istringstream iss(buffer);
+        std::vector<std::string> fromSer;
+        while(!iss.eof()) {
+            int i = 1;
+            std::string temp;
+            iss >> temp;
+            std::cout << temp << "\n";
+            std::string delim = ",";
+            auto start = 0U;
+            auto end = temp.find(delim);
+            while (end != std::string::npos)
+            {
+                fromSer.push_back(temp.substr(start, end - start));
+                start = static_cast<unsigned int>(end + delim.length());
+                end = temp.find(delim, start);
+            }
+            std::map<std::string, std::string>::iterator mapIndx;
+            for (mapIndx = mapH.getvartobindMap()->begin(); mapIndx != mapH.getvartobindMap()->end(); ++mapIndx) {
+                int inxd = findIndexFromString(mapIndx->second);
+                if (inxd != -1) {
+                    mapH.getsymblTable()->at(mapIndx->second) = std::stoi(fromSer.at(inxd));
+                }
+            }
+        }
+
+        /* Write a response to the client */
+        n = write(sockeNum, "I got your message", 18);
+
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+
+
+
+        usleep(static_cast<__useconds_t>(sleepTime));
+    }
+}
+
+int OpenServerCommand::findIndexFromString(std::string const &path) const {
+    ptrdiff_t pos = find(this->DirectVar.begin(), this->DirectVar.end(), path) - this->DirectVar.begin();
+    if (pos >= this->DirectVar.size()) {
+        perror("PATH NOT FOUND IN VECTOR");
+        return -1;
+    }
+    return static_cast<int>(pos);
 }
 
 
--- src/OpenServerCommand.h
+++ src/OpenServerCommand.h
@@ -9,7 +9,29 @@
 #include "Command.h"
 
 class OpenServerCommand:public Command{
-    std::vector<std::string> fromServerToMap;
+    std::vector<std::string> DirectVar = {"/instrumentation/airspeed-indicator/indicated-speed-kt",
+                                    "/instrumentation/altimeter/indicated-altitude-ft",
+                                    "/instrumentation/altimeter/pressure-alt-ft",
+                                    "/instrumentation/attitude-indicator/indicated-pitch-deg",
+                                    "/instrumentation/attitude-indicator/indicated-roll-deg",
+                                    "/instrumentation/attitude-indicator/internal-pitch-deg",
+                                    "/instrumentation/attitude-indicator/internal-roll-deg",
+                                    "/instrumentation/encoder/indicated-altitude-ft",
+                                    "/instrumentation/encoder/pressure-alt-ft",
+                                    "/instrumentation/gps/indicated-altitude-ft",
+                                    "/instrumentation/gps/indicated-ground-speed-kt",
+                                    "/instrumentation/gps/indicated-vertical-speed",
+                                    "/instrumentation/heading-indicator/indicated-heading-deg",
+                                    "/instrumentation/magnetic-compass/indicated-heading-deg",
+                                    "/instrumentation/slip-skid-ball/indicated-slip-skid",
+                                    "/instrumentation/turn-indicator/indicated-turn-rate",
+                                    "/instrumentation/vertical-speed-indicator/indicated-speed-fpm",
+                                    "/controls/flight/aileron",
+                                    "/controls/flight/elevator",
+                                    "/controls/flight/rudder",
+                                    "/controls/flight/flaps",
+                                    "/controls/engines/engine/throttle",
+                                    "/engines/engine/rpm"};
 public:
     void execute() override;
 
@@ -18,6 +40,10 @@ public:
     std::string stringify() override;
 
     void addMaps(mapHandler &mapHandler1) override;
+
+    void connectAndUpdate(int sleepTime, int sockeNum);
+
+    int findIndexFromString(std::string const &path) const;
 
 };
 
